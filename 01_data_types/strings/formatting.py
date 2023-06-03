b7f5d5b76be524d2e1b6f2a47f8f178e61cbc0cd
Josh Hayles <josh@joshhayles.com>
2023-06-03 10:51:49 -0600

    formatting string input

---
+++ 01_data_types/strings/formatting.py
@@ -0,0 +1,17 @@
+# format a result (i.e. 1000) to look like 1,000
+
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+
+# z = round(x + y)
+
+# print(f"{z:,}") # -> 3,002
+
+# determine the number of digits you want to print
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = round(x / y)
+
+print(f"{z:.2f}")  # -> 1.00
