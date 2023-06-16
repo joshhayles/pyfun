0e53098c0dd218f28d914829410633ee9657f61b
Josh Hayles <josh@joshhayles.com>
2023-06-16 06:51:21 -0600

    list_iteration

---
+++ 02_control_flow/iteration/list_iteration.py
@@ -0,0 +1,13 @@
+# To iterate over the indices of a sequence, you can combine range() and len() as follows:
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+
+for i in range(len(a)):
+    print(i, a[i])
+    
+
+# 0 Mary
+# 1 had
+# 2 a
+# 3 little
+# 4 lamb
