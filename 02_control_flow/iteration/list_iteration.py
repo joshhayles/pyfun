a7af42487006887d3fc6400f195f7ced24b6d464
Josh Hayles <josh@joshhayles.com>
2023-06-16 07:18:04 -0600

    enumerate()

--- 02_control_flow/iteration/list_iteration.py
+++ 02_control_flow/iteration/list_iteration.py
@@ -4,7 +4,22 @@ a = ['Mary', 'had', 'a', 'little', 'lamb']
 
 for i in range(len(a)):
     print(i, a[i])
-    
+
+
+# 0 Mary
+# 1 had
+# 2 a
+# 3 little
+# 4 lamb
+
+
+#  You can do the same thing using enumerate():
+
+b = ['Mary', 'had', 'a', 'little', 'lamb']
+
+for index, item in enumerate(b):
+    print(index, item)
+
 
 # 0 Mary
 # 1 had
